d1caa0a0485b9a69b885aa528830d1ec34b2c6ad
Ulf Müller <ulmuelle@gmail.com>
2017-08-17 21:39:40 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# clojure2
+
+First baby steps in clojure
+
+## Usage
+
+nothing usefule to use here
+
+## License
+
+Copyright © 2017 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/clojure2/first-steps.clj
@@ -0,0 +1,60 @@
+(ns clojure2.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn my-func
+  "This is the documentation of my famous new function"
+  [arg-a arg-b]
+  (println "adding a and b results in: " (+ arg-a arg-b))
+  )
+(my-func 1 2)
+(def abc "abcdefghijklmnopqrstuvxyz")
+(println abc)
+(def rot13 "mnopqrstuvxyzabcdefghijkl")
+(def encoding "mnopqrstuvxyzabcdefghijkl")
+(use '[clojure.string :only [index-of]])
+(index-of abc "a")
+(index-of abc "f")
+(def alphabet "abcdefghijklmnopqrstuvxyz")
+(index-of alphabet "f")
+(get encoding (index-of alphabet "f"))
+(get encoding (index-of alphabet "a"))
+(defn
+  "Encodes a single character in rot13 fashion"
+  [the-char]
+  (get encoding (index-of alphabet the-char))
+  RuntimeException EOF while reading, starting at line 1  clojure.lang.Util.runtimeException (Util.java:221)
+  (defn
+    "Encodes a single character in rot13 fashion"
+    [the-char]
+    (get encoding (index-of alphabet the-char))
+    )
+  (defn
+    "Encodes a single character in rot13 fashion"
+    [my-char]
+    (get encoding (index-of alphabet my-char))
+    )
+  IllegalArgumentException First argument to defn must be a symbol  clojure.core/defn--4387 (core.clj:294)
+  (defn encode-char
+    "Encodes a single character in rot13 fashion"
+    [my-char]
+    (get encoding (index-of alphabet my-char))
+    )
+  (defn rot13
+    "implements rot13 cypher"
+    [text]
+    (map encode-char "helloworld"))
+  (defn rot13
+    "implements rot13 cypher"
+    [text]
+    (map encode-char text))
+  (rot13 "hello")
+  (defn encode-char
+    "Encodes a single character in rot13 fashion"
+    [^Character my-char]
+    (get encoding (index-of alphabet my-char))
+    )
+  (rot13 "hello")
